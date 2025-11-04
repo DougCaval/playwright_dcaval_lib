72658c470c824205b88f94b84feaf138f94badea
CavalITxp <cavalmaison@gmail.com>
2025-11-04 13:26:11 -0300

    fix: update Page import

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // index.ts na sua lib
 export * from '@playwright/test';
 export { chromium, firefox, webkit } from 'playwright';
+export type { Page } from 'playwright';
 
 // Custom helpers
 export const dcavalHelper = () => console.log('Lib customizada por Doug Caval!');
